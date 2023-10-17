d3abbf915f5dd3cd54cef6d91509c0e57b28df1b
오은희 <72530436+eundol0519@users.noreply.github.com>
2023-10-17 15:46:01 +0900

    📝 : 룰 수정

--- src/pages/Rules.tsx
+++ src/pages/Rules.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../elements/Button";
+import { css } from "@emotion/react";
 
 const Rules = () => {
   const navigation = useNavigate();
@@ -7,26 +8,41 @@ const Rules = () => {
   return (
     <div>
       <h2>Game Rules</h2>
-      <ul>
-        <li>게임을 진행할 숫자 갯수를 선택한다.</li>
-        <li>중복되지 않은 숫자를 랜덤으로 생성 받는다.</li>
-        <li>게임을 시작한다.</li>
+      <ol>
         <li>
-          사용자가 숫자를 입력하면 정답 숫자와 사용자가 입력한 숫자를 비교한다.
+          게임 시작 전 게임 진행을 위한 옵션을 선택한다.
           <br />
-          (아래 조건에 맞춰 비교한다.)
+          옵션에서는 숫자 개수, 라운드 수를 선택하게 된다.
+        </li>
+        <li>옵션 선택이 끝났다면 시작하기를 눌러 게임을 시작한다.</li>
+        <li>
+          기본적으로 10라운드로 진행되며, 옵션에서 최대 50라운드까지 설정할 수
+          있다.
+        </li>
+        <li>
+          숫자를 입력하면 아래의 조건에 맞춰 입력한 숫자와 결과가 계산된다.
         </li>
         <ul>
-          <li>
-            숫자 포함 / 위치 일치 = 1 Strike 숫자 포함 / 위치 불일치 = 1 Ball
-            숫자
-          </li>
-          <li>
-            미포함 = 1 Out 10 라운드까지 진행하고, 그때까지 정답을 맞추지 못하면
-          </li>
-          <li>게임 실패 10 라운드 이내에 정답을 맞추면 게임 성공</li>
+          <li>Strike : 숫자와 위치가 전부 맞을 경우</li>
+          <li>Ball : 숫자는 맞지만 위치가 틀렸을 경우</li>
+          <li>Out : 숫자와 위치가 전부 틀릴 경우</li>
         </ul>
-      </ul>
+        <li>
+          설정한 라운드 내에 정답을 맞출 경우 성공, 맞추지 못할 경우 실패로
+          게임을 마친다.
+        </li>
+        <br />
+        <span
+          css={css`
+            color: red;
+            margin-left: -20px;
+            font-size: 14px;
+          `}
+        >
+          ※ 정답은 0~9 사이에 중복되지 않은 랜덤 숫자입니다.
+        </span>
+      </ol>
+
       <Button
         onClick={() => {
           navigation("/");
